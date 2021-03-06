c31b0742a67a6a4eb8361cc3a0dcea1558f6736d
aukustik <root@HOME-PC.localdomain>
2021-03-06 14:13:18 +0700

    states output added

--- salt_ms_checker.py
+++ salt_ms_checker.py
@@ -7,53 +7,63 @@ class MsChecker:
     def __init__(self):
         self.ms_type = ''
         self.input_file = ''
+        self.local_salt = client.LocalClient()
+        self.input_list = []
+        self.available_minions = []
+        self.available_minions_ids = []
+        self.failed_minions_ids = []
+        
 
     def run(self, minions):
 
-        _local_salt = client.LocalClient()
-
         # Проверка активности миньонов
-        _temp = _local_salt.cmd(minions, 'test.ping', tgt_type='list', timeout=5)
-        _available_minions = []
+        print('Waiting for a response from minions...')
+        self.input_list = self.local_salt.cmd(minions, 'test.ping', tgt_type='list', timeout=5)
 
-        for minion in _temp.keys():
-            if _temp[minion]:
-                _available_minions.append(Minion(minion))
+        for minion in self.input_list.keys():
+            if self.input_list[minion]:
+                self.available_minions.append(Minion(minion))
 
-        if _available_minions:
-            self.get_minions_data(_available_minions, _local_salt)
+        self.available_minions_ids = self.get_minions_ids(self.available_minions)
+        self.failed_minions_ids = self.get_failed_minions(
+            self.input_list,
+            self.available_minions_ids)
+        
+        if self.failed_minions_ids:
+            self.print_failed_minions(self.failed_minions_ids)
+
+        if self.available_minions:
+            self.get_minions_data()
             _report = ''
-            for minion in _available_minions:
+            for minion in self.available_minions:
                 _report += minion.get_info()
-                # _report += str(minion.get_results())
                 _report += '\n\n'
-            print(_report)
+            # print(_report)
             self.write_to_report_file(_report)
         else:
             print('No available minions')
 
-# Собираем инфу с миньонов
-    def get_minions_data(self, available_minions, local_salt):
+    def print_failed_minions(self, failed_minions_ids):
+        print('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        print('These minions didn\'t answer:')
+        for minion_id in failed_minions_ids:
+            print('\t\'{}\''.format(minion_id))
+        print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
 
-        _minions_ids = self.get_minions_ids(available_minions) 
-        _grains_dict = local_salt.cmd(_minions_ids, 'grains.items', tgt_type='list')
-        _fstab_dict = local_salt.cmd(_minions_ids, 'mount.fstab', tgt_type='list')
-        _cpu_info_dict = local_salt.cmd(_minions_ids, 'status.cpuinfo', tgt_type='list')
-        _disk_usage = local_salt.cmd(_minions_ids, 'disk.usage', tgt_type='list')
+# Собираем инфу с миньонов
+    def get_minions_data(self):
+        print('\nData collection of available minions begins...\n')
+        _grains_dict = self.get_grains_dict(self.available_minions_ids)
+        _fstab_dict = self.get_fstab_dict(self.available_minions_ids)
+        _cpu_info_dict = self.get_cpu_info_dict(self.available_minions_ids)
+        _disk_usage = self.get_disk_usage_dict(self.available_minions_ids)
         
-        for minion in available_minions:
+        for minion in self.available_minions:
             minion.grains = _grains_dict[minion.minion_id]
             minion.fstab = _fstab_dict[minion.minion_id]
             minion.disk_usage = _disk_usage[minion.minion_id]
             minion.cpu_info = _cpu_info_dict[minion.minion_id]
             minion.set_info_by_ms_type(self.ms_type)
-
-# Формируем список minion_id из созданных объектов
-    def get_minions_ids(self, available_minions):
-        _minions_ids = []
-        for minion in available_minions:
-            _minions_ids.append(minion.minion_id)
-        return _minions_ids
 
 # Получаем список миньонов из файла
     def get_minions_from_file(self, filename):
@@ -67,10 +77,53 @@ class MsChecker:
         
         return _minions
 
+    def get_failed_minions(self, full_list, avail_list):
+        
+        _failed = list(set(full_list) - set(avail_list))
+
+        return _failed
+
+# Формируем список minion_id из созданных объектов
+    def get_minions_ids(self, available_minions):
+        _minions_ids = []
+        for minion in available_minions:
+            _minions_ids.append(minion.minion_id)
+        return _minions_ids
+
+    def get_grains_dict(self, minions_ids):
+
+        _grains_dict = self.local_salt.cmd(minions_ids, 'grains.items', tgt_type='list')
+        print('grains.items received from minions')
+
+        return _grains_dict
+    
+    def get_fstab_dict(self, minions_ids):
+
+        _fstab_dict = self.local_salt.cmd(minions_ids, 'mount.fstab', tgt_type='list')
+        print('mount.fstab received from minions')
+
+        return _fstab_dict
+    
+    def get_cpu_info_dict(self, minions_ids):
+
+        _cpu_info_dict = self.local_salt.cmd(minions_ids, 'status.cpuinfo', tgt_type='list')
+        print('status.cpuinfo received from minions')
+
+        return _cpu_info_dict
+    
+    def get_disk_usage_dict(self, minions_ids):
+
+        _disk_usage_dict = self.local_salt.cmd(minions_ids, 'disk.usage', tgt_type='list')
+        print('disk.usage received from minions')
+
+        return _disk_usage_dict
+
 # Выводим Отчет в файл
     def write_to_report_file(self, report):
-        _report = open("issues_report.txt", "w")
+        _file_name = 'issues_report.txt'
+        _report = open(_file_name, "w")
         _report.write(report)
+        print('The report was written to a file {}\n'.format(_file_name))
         _report.close()
 
 if __name__ == '__main__':
@@ -87,6 +140,5 @@ if __name__ == '__main__':
         print('Not Enough Args')
         sys.exit()
 
-    print(ms_checker.get_minions_from_file(ms_checker.input_file))
     ms_checker.run(ms_checker.get_minions_from_file(ms_checker.input_file))
 
